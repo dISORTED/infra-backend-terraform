21b26e4f5af6740c3dfa4cc7f22f5e99503f887b
dISORTED <117547716+dISORTED@users.noreply.github.com>
2025-08-14 12:44:20 +0000

    Arreglo

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,37 @@ resource "docker_container" "fastapi_container" {
   name  = var.container_name
   image = docker_image.fastapi_image.name
 
+  env = [
+    "DB_HOST=postgres-db",
+    "DB_PORT=5432",
+    "DB_USER=${var.postgres_user}",
+    "DB_PASSWORD=${var.postgres_password}",
+    "DB_NAME=${var.postgres_db}",
+  ]
+
   ports {
     internal = var.container_port
     external = var.host_port
   }
+
+  depends_on = [docker_container.postgres]
 }
+
+resource "docker_container" "postgres" {
+  name    = var.postgres_container_name
+  image   = "postgres:15"
+  restart = "always"
+
+  env = [
+    "POSTGRES_USER=${var.postgres_user}",
+    "POSTGRES_PASSWORD=${var.postgres_password}",
+    "POSTGRES_DB=${var.postgres_db}",
+  ]
+
+  ports {
+    internal = 5432
+    external = 5432
+  }
+}
+
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,5 +13,26 @@ variable "container_port" {
 variable "host_port" {
   description = "Puerto externo expuesto en el host"
   type        = number
-  default     = 8000
+  default     = 8080
 }
+variable "postgres_container_name" {
+  description = "Nombre del contenedor de PostgreSQL"
+  type        = string
+  default     = "postgres-db"
+}
+
+variable "postgres_user" {
+  type    = string
+  default = "admin"
+}
+
+variable "postgres_password" {
+  type    = string
+  default = "admin123"
+}
+
+variable "postgres_db" {
+  type    = string
+  default = "app_db"
+}
+
